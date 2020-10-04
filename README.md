8aeb440a98de29d193b2922177d6566fef15d4f7
ldmsys <16889349+ldmsys@users.noreply.github.com>
2020-10-04 12:45:31 +0900

    refactor: asynchorize functions

--- README.md
+++ README.md
@@ -1,26 +1,24 @@
 # buffer-server 
  * Simple and single file host by Buffer's data
 
-## Notice
- * This project is subproject of FADe Project
- * Internally used by FADe, Licensed under same license of FADe.
-
 ## Usage
  * Install project:
 ```
- $ npm install @fade-project/buffer-server
+ $ npm install buffer-server
 ```
- * Put in your project:
+ * require() in your project:
 ```
 var buffer_server = require('@fade-project/buffer-server')
 ```
- * buffer_server.sftp_server(serverKey, allowedUser, allowedPass, filename, filedata[, ret_port]);
+ * buffer_server.sftp_server(serverKey, allowedUser, allowedPass, filename, filedata);
    * Accept Parameters as a String, except filedata (it's a Buffer).
  * buffer_server.web_server(index, filename, filedata[, ret_port]);
    * Also Accept Parameters as a String, except filedata (it's also a buffer).
- * In default, web_server() returns Port Number. You may change by set ret_port option to false
- * In default, sftp_server() returns Promise(Will resolved to Port Number), You may change by ret_port option to false
+ * web_server() and sftp_server() returns Promise. It will resolve to Buffer. (TS: Promise<Buffer>)
 
 ## Credit
  * [express](https://github.com/expressjs/express) - [MIT License](https://github.com/expressjs/express/blob/master/LICENSE)
  * [ssh2](https://github.com/mscdex/ssh2) - [MIT License](https://github.com/mscdex/ssh2/blob/master/LICENSE)
+
+## License
+ * [MIT License](LICENSE)
--- buffer-server.js
+++ buffer-server.js
@@ -5,115 +5,107 @@ const express = require('express');
 const app = express();
 
 /* buffer-server Module
- * This Subproject is part of FADe Project
- * Copyright (C) ldmsys, License subject by main project
- * (Currently MIT License)
+ * Copyright (C) ldmsys, Licensed under MIT License.
  */
 
-exports.sftp_server = (serverKey, allowedUser, allowedPass, filename, filedata, ret_port = true) => {
-/*
- * Universalized, Stripped-down version of
- * https://github.com/mscdex/ssh2/blob/master/examples/sftp-server-download-only.js
- * Thanks for awesome ssh2 module.
- */
-	var port = 0;
-	var server = new ssh2.Server({
-	  hostKeys: [serverKey]
-	}, (client) => {
-	  client.on("authentication", (ctx) => {
-		// Authentication
-		if(ctx.method == "password" && ctx.user.length == allowedUser.length && crypto.timingSafeEqual(Buffer.from(ctx.user), Buffer.from(allowedUser))
-		&& ctx.password.length == allowedPass.length && crypto.timingSafeEqual(Buffer.from(ctx.password), Buffer.from(allowedPass))) {
-		  ctx.accept();
-		}else{
-		  ctx.reject(['password']);
-		}
-	  }).on('ready', () => {
-		// Ready
-		client.on('session', (accept, reject) => {
-		  var session = accept();
-		  
-		  session.on('sftp', (accept, reject) => {
-			// SFTP Connection
-			var sftpStream = accept();
-			var openFiles = {};
-			var handleCount = 0;
-			function onSTAT(reqid, path) {
-			  if (path !== '/'+filename)
-				return sftpStream.status(reqid, ssh2.SFTP_STATUS_CODE.FAILURE);
-				var mode = constants.S_IFREG; // Regular file
-				mode |= constants.S_IRWXU; // read, write, execute for user
-				mode |= constants.S_IRWXG; // read, write, execute for group
-				mode |= constants.S_IRWXO; // read, write, execute for other
-			  sftpStream.attrs(reqid, {
-				mode: mode,
-				uid: 0,
-				gid: 0,
-				size: filedata.length,
-				atime: Date.now(),
-				mtime: Date.now()
-			  });
+exports.sftp_server = async (serverKey, allowedUser, allowedPass, filename, filedata) => {
+	/*
+	* Universalized, Stripped-down version of
+	* https://github.com/mscdex/ssh2/blob/master/examples/sftp-server-download-only.js
+	* Thanks for awesome ssh2 module.
+	*/
+	return new Promise((res, rej) => {
+		var server = new ssh2.Server({
+		hostKeys: [serverKey]
+		}, (client) => {
+		client.on("authentication", (ctx) => {
+			// Authentication
+			if(ctx.method == "password" && ctx.user.length == allowedUser.length && crypto.timingSafeEqual(Buffer.from(ctx.user), Buffer.from(allowedUser))
+			&& ctx.password.length == allowedPass.length && crypto.timingSafeEqual(Buffer.from(ctx.password), Buffer.from(allowedPass))) {
+			ctx.accept();
+			}else{
+			ctx.reject(['password']);
 			}
-			var hl = (filedata.length+1>256)?256:filedata.length+1;
-			sftpStream.on('OPEN', (reqid, reqFilename, flags, attrs) => {
-			  if (reqFilename !== '/'+filename || !(flags & ssh2.SFTP_OPEN_MODE.READ))
-				return sftpStream.status(reqid, ssh2.SFTP_STATUS_CODE.FAILURE);
-			  var handle = Buffer.alloc(hl);
-			  openFiles[handleCount] = { read: false };
-			  handle.writeUInt32BE(handleCount++, 0, true);
-			  sftpStream.handle(reqid, handle);
-			}).on('READ', (reqid, handle, offset, length) => {
-			  if (handle.length !== hl || !openFiles[handle.readUInt32BE(0, true)])
-				return sftpStream.status(reqid, ssh2.SFTP_STATUS_CODE.FAILURE);
-			  //var state = openFiles[handle.readUInt32BE(0, true)];
-			  if (offset >= filedata.length)
-				sftpStream.status(reqid, ssh2.SFTP_STATUS_CODE.EOF);
-			  else {
-				//state.read = true;
-				sftpStream.data(reqid, filedata.slice(offset, offset+handle.length));
-			  }
-			}).on('CLOSE', (reqid, handle) => {
-			  var fnum;
-			  if (handle.length !== hl || !openFiles[(fnum = handle.readUInt32BE(0, true))])
-				return sftpStream.status(reqid, ssh2.SFTP_STATUS_CODE.FAILURE);
-			  delete openFiles[fnum];
-			  sftpStream.status(reqid, ssh2.SFTP_STATUS_CODE.OK);
-			}).on('REALPATH', (reqid, path) => {
-			  sftpStream.name(reqid, {filename: "/"});
-			}).on('STAT', onSTAT)
-			.on('LSTAT', onSTAT);
-		  });
+		}).on('ready', () => {
+			// Ready
+			client.on('session', (accept, reject) => {
+			var session = accept();
+			
+			session.on('sftp', (accept, reject) => {
+				// SFTP Connection
+				var sftpStream = accept();
+				var openFiles = {};
+				var handleCount = 0;
+				function onSTAT(reqid, path) {
+				if (path !== '/'+filename)
+					return sftpStream.status(reqid, ssh2.SFTP_STATUS_CODE.FAILURE);
+					var mode = constants.S_IFREG; // Regular file
+					mode |= constants.S_IRWXU; // read, write, execute for user
+					mode |= constants.S_IRWXG; // read, write, execute for group
+					mode |= constants.S_IRWXO; // read, write, execute for other
+				sftpStream.attrs(reqid, {
+					mode: mode,
+					uid: 0,
+					gid: 0,
+					size: filedata.length,
+					atime: Date.now(),
+					mtime: Date.now()
+				});
+				}
+				var hl = (filedata.length+1>256)?256:filedata.length+1;
+				sftpStream.on('OPEN', (reqid, reqFilename, flags, attrs) => {
+				if (reqFilename !== '/'+filename || !(flags & ssh2.SFTP_OPEN_MODE.READ))
+					return sftpStream.status(reqid, ssh2.SFTP_STATUS_CODE.FAILURE);
+				var handle = Buffer.alloc(hl);
+				openFiles[handleCount] = { read: false };
+				handle.writeUInt32BE(handleCount++, 0, true);
+				sftpStream.handle(reqid, handle);
+				}).on('READ', (reqid, handle, offset, length) => {
+				if (handle.length !== hl || !openFiles[handle.readUInt32BE(0, true)])
+					return sftpStream.status(reqid, ssh2.SFTP_STATUS_CODE.FAILURE);
+				//var state = openFiles[handle.readUInt32BE(0, true)];
+				if (offset >= filedata.length)
+					sftpStream.status(reqid, ssh2.SFTP_STATUS_CODE.EOF);
+				else {
+					//state.read = true;
+					sftpStream.data(reqid, filedata.slice(offset, offset+handle.length));
+				}
+				}).on('CLOSE', (reqid, handle) => {
+				var fnum;
+				if (handle.length !== hl || !openFiles[(fnum = handle.readUInt32BE(0, true))])
+					return sftpStream.status(reqid, ssh2.SFTP_STATUS_CODE.FAILURE);
+				delete openFiles[fnum];
+				sftpStream.status(reqid, ssh2.SFTP_STATUS_CODE.OK);
+				}).on('REALPATH', (reqid, path) => {
+				sftpStream.name(reqid, {filename: "/"});
+				}).on('STAT', onSTAT)
+				.on('LSTAT', onSTAT);
+			});
+			});
+		}).on("error", (e) => {
+			if(e.code !== 'ECONNRESET') {
+				console.error(e);
+				process.exit(1);
+			} // Ignore ECONNRESET Error
 		});
-	  }).on("error", (e) => {
-		  if(e.code !== 'ECONNRESET') {
-			  console.error(e);
-			  process.exit(1);
-		  } // Ignore ECONNRESET Error
-	  });
-	}).listen(0, "0.0.0.0", () => {
-		port = server.address().port;
+		}).listen(0, "0.0.0.0", () => {
+			res(server.address().port);
+		});
 	});
-	if(!ret_port) {
-		return server;
-	}else{
-		return new Promise((res, rej) => {
-			setInterval(() => {
-				if(port !== 0) res(port);
-			}, 250);
-		});
-	}
  }
 
-exports.web_server = (index, filename, filedata, ret_port = true) => {
-    app.get('/', (req, res) => {
-	  res.send(index);
-	}).get('/'+filename, (req, res) => {
-	  res.writeHead(200, {
-		'Content-Disposition': `attachment; filename="${filename}"`,
-		'Content-Type': "application/octet-stream"
-	  });
-	  res.end(filedata);
-	})
-	var server = app.listen(0, () => {});
-	return ret_port ? server.address().port : app;
+exports.web_server = async (index, filename, filedata) => {
+	return new Promise((res, rej) => {
+		app.get('/', (req, res) => {
+			res.send(index);
+		}).get('/'+filename, (req, res) => {
+			res.writeHead(200, {
+				'Content-Disposition': `attachment; filename="${filename}"`,
+				'Content-Type': "application/octet-stream"
+			});
+			res.end(filedata);
+		});
+		var server = app.listen(0);
+		res(server.address().port);
+	});
 }
